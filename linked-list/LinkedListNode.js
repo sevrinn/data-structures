2ad9a05e102b202fa98189a7c1936c30ac5a9d7f
Sevrinn <sevrinnsevrinn@gmail.com>
2022-05-09 13:10:59 -0700

    creates and exports LinkedListNode class

---
+++ linked-list/LinkedListNode.js
@@ -0,0 +1,21 @@
+export default class LinkedListNode {
+	constructor(value, next = null) {
+		this.value = value
+		this.next = next
+	}
+
+	toString(callback) {
+		return callback ? callback(this.value) : `${this.value}`
+	}
+}
+
+console.log(typeof LinkedListNode)
+
+/**
+ * My attempt at breaking this down line by line
+ * 1: defines a L.L.Node class. (and exports it with export default)
+ * 2: When an instance of node is created, the constructor func is called, initializing two properties:
+ *    - value
+ *    - a "pointer" named next (with a default value of null)
+ * 7: LLNode is created with a toString node
+ */
